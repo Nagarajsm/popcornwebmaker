66137aab42ff3763143c1de15c12fc8d59126370
Nagaraj <nagaraj@brizzlabs.com>
2013-05-14 16:10:33 +0530

    loading image on screen

--- index.php
+++ index.php
@@ -2,13 +2,32 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="css/popcorn.css">
-<!-- test comment out git -->
+<!-- test commen out git -->
 <script type="text/javascript">
 
 function loadvideofile(){
 	var finename = document.getElementById('txtarea').value;
 	document.getElementById('videotag').src=finename;
 }
+ 
+function handleFileSelect(evt) {
+    var files = evt.target.files; 
+    for (var i = 0, f; f = files[i]; i++) {
+    var reader = new FileReader();
+    reader.onload = (function(theFile) {  
+        return function(e) {
+          document.getElementById('list').src =  e.target.result;
+        };
+      })(f);
+    reader.readAsDataURL(f);
+    }
+  }
+
+function loadImagefile(){
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+}
+  
+
 </script>
 
 
@@ -21,12 +40,17 @@ function loadvideofile(){
   		<div class="bodysection">
   			<div class="playersection">
   				<div class="player">
-  					<div id="vvv" class="insideplayer">
-  					<video id="videotag" loop controls src="" style="width:100%;height:100%;visibility:none;">
-  					
-  					</video>
+            
+            <div  class="imageload" >
+              <img id="list" scr=''/>
+            </div>
+            
+            <div class="insideplayer">
+    				  <video id="videotag" loop controls src="" style="width:100%;height:100%;visibility:none;">
+     					</video>
   					</div>
-  				</div>
+  				
+          </div>
     		</div>
     		
     		<div class="leftmenusection">
@@ -40,14 +64,15 @@ function loadvideofile(){
     			</div>
 
     			<div>
-    				<h4 id="linktext">Paste a link from YouTube, SoundCloud, Vimeo or an HTML5 video<h4>
+    				  <h4 id="linktext">Paste a link from YouTube, SoundCloud, Vimeo or an HTML5 video<h4>
     					
     					<TEXTAREA  id="txtarea">http://ads.brizztv.com/movies/1.webm</TEXTAREA>
     					<br><br><br>
-    					<input id="loadbtn" type="button" name="Load" value="Load Clip" onClick="loadvideofile();"/> 
+    					<input id="loadbtn" type="button" name="Load" value="Load Clip" onClick="loadvideofile();"/>
+              <br><br><br>
+             
+              <input id="files" type="file" name="files[]" value="Load Image" multiple  onClick="loadImagefile();"/>
     			</div>
-    			
-				
     		</div>
   		</div>
     	
